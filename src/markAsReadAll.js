1dae5d81f4fdf4e5f069611f5c64e0eb8b7807bc
raza210 <50223841-raza210@users.noreply.replit.com>
2025-11-26 07:08:03 +0000

    Initial commit

---
+++ src/markAsReadAll.js
@@ -0,0 +1,55 @@
+"use strict";
+
+const utils = require("../utils");
+const log = require("npmlog");
+
+module.exports = function (defaultFuncs, api, ctx) {
+  return function markAsReadAll(callback) {
+    let resolveFunc = function () {};
+    let rejectFunc = function () {};
+    const returnPromise = new Promise(function (resolve, reject) {
+      resolveFunc = resolve;
+      rejectFunc = reject;
+    });
+
+    if (!callback) {
+      callback = function (err, friendList) {
+        if (err) {
+          return rejectFunc(err);
+        }
+        resolveFunc(friendList);
+      };
+    }
+
+    const form = {
+      folder: "inbox",
+    };
+
+    defaultFuncs
+      .post(
+        "https://www.facebook.com/ajax/mercury/mark_folder_as_read.php",
+        ctx.jar,
+        form,
+      )
+      .then(utils.saveCookies(ctx.jar))
+      .then(utils.parseAndCheckLogin(ctx, defaultFuncs))
+      .then(function (resData) {
+        if (resData.error) {
+          // Gracefully handle error 1357031 (content not available)
+          if (resData.error === 1357031) {
+            log.warn("markAsReadAll", "Some content is no longer available, skipping.");
+            return callback();
+          }
+          throw resData;
+        }
+
+        return callback();
+      })
+      .catch(function (err) {
+        log.error("markAsReadAll", err);
+        return callback(err);
+      });
+
+    return returnPromise;
+  };
+};
