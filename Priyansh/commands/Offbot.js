f761c7a301b68bb5f522adad92efd92d1431c707
Shaan Khan K <pip000470@gmail.com>
2025-12-02 22:46:03 +0500

    Update Offbot.js

--- Priyansh/commands/Offbot.js
+++ Priyansh/commands/Offbot.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "offbot",
 	version: "1.0.0",
 	hasPermssion: 2,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+	credits: "SHAAN KHAN",
 	description: "turn the bot off",
 	commandCategory: "system",
 	cooldowns: 0
